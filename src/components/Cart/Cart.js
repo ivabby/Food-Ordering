f820978fc9ab050b4c1a2840acdda3d5d2c857d8
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2021-11-20 20:04:45 +0530

    Food Ordering App

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,57 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({
+      id: item.id,
+      name: item.name,
+      amount: 1,
+      price: item.price,
+    });
+  };
+
+  const cartItems = cartCtx.items.map((item) => {
+    return (
+      <CartItem
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+      ></CartItem>
+    );
+  });
+
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onShowCart={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Meals Image" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
